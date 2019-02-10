79022f2578119071a3ecbe28be773d1cdfa53b79
JAE HOON CHOI <hoonja@gmail.com>
2019-02-10 22:34:36 +0900

    ~ 3-4

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../components/Home'
+import Login from '../components/Login'
+import Board from '../components/Board'
+import Card from '../components/Card'
+import NotFound from '../components/NotFound'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/b/:bid', component: Board, children: [
+    { path: 'c/:cid', component: Card }
+  ]},
+  { path: '*', component: NotFound }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
